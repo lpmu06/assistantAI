e773c00678c2b773561adb3e3f6371a86ca0b4e5
lpmu06 <lpmu06@gmail.com>
2024-06-17 22:34:29 -0300

    sidebar changes

--- app_EDA.py
+++ app_EDA.py
@@ -17,6 +17,12 @@ st.header('Exploratory Data Analysis Part')
 st.subheader('Solution')
 st.write('Hello, I am your AI assistant and I am here to help you with your data science projects.')
 
+#Explanation sidebar
 with st.sidebar:
-    st.write('''
-             ''')
+    st.write('*Your Data Science Adventure Begins with an CSV File.*')
+    st.caption('''**You may already know that every exciting data science journey starts with a dataset.
+    That's why I'd love for you to upload a CSV file.
+    Once we have your data in hand, we'll dive into understanding it and have some fun exploring it.
+    Then, we'll work together to shape your business challenge into a data science framework.
+    I'll introduce you to the coolest machine learning models, and we'll use them to tackle your problem. Sounds fun right?**
+    ''')
